3326ca47407b8386f2a4d82eb5ffc36487ff7705
Vasya1988 <myhappy1988@gmail.com>
2024-05-22 14:19:54 +0300

    fixed if open url

--- app/info/[name]/page.tsx
+++ app/info/[name]/page.tsx
@@ -7,16 +7,51 @@ import Modal from "../../components/modal/modal";
 
 const Info = () => {
 
-    const {info} = useContext(MyContext);
+    const {info, setInfo} = useContext(MyContext);
     const router = useRouter()
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const currentHash = window.location.href.split('/')
+    const splitHash = currentHash[currentHash.length - 1]
 
+    console.log(splitHash)
+    // функция запроса к api
+    const getApiInfo = async (props?: string) => {
+
+        const url = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party';
+        const token = '945042a7822c0a45fe52fe8ee1cbc191cfca281f';
+        let query = props || '7721705169'; // 7721705169, 7707083893
+
+        const options: RequestInit = {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": "Token " + token
+            },
+            body: JSON.stringify({ query: query })
+        }
+
+        const response = await fetch(url, options);
+
+        const result = await response.json()
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+        
+        // Обновляем состояние Info
+        setInfo(result.suggestions[0]);
+
+        return result
+    };
     // Проверка, если info не корректное, вернемся на главную страницу
     useEffect(() => {
         if(info === undefined || Object.keys(info).length === 0) {
-            router.push('/')
+            // router.push('/')
+            getApiInfo(splitHash)
         }
-    }, [info, router])
+    }, [info])
 
     // Открываем модальное окно
     const HandleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
